67757e66daf30c4cff8f0128cfd15812091ab89e
Loyalty <lohunmule@gmail.com>
2025-12-30 16:35:28 +0100

    Additions

--- controllers/authController.js
+++ controllers/authController.js
@@ -185,9 +185,10 @@ exports.forgotPassword = async (req, res) => {
 
     // Nodemailer transporter
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      // host: "smtp.gmail.com",
+      // port: 587,
+      // secure: false,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
